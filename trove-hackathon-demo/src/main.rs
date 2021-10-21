1ddf9285f9e6d810883cd7b0643c2e6e5e426b1a
Ankush Agarwal <ankushagarwal11@gmail.com>
2021-10-20 17:07:36 -0700

    Create test accounts

--- trove-hackathon-demo/src/main.rs
+++ trove-hackathon-demo/src/main.rs
@@ -4,6 +4,7 @@
 use anyhow::{Context, Result};
 use diem_config::config::NodeConfig;
 use diem_crypto::PrivateKey;
+use diem_sdk::types::account_config::xus_tag;
 use diem_sdk::{
     client::BlockingClient,
     transaction_builder::{Currency, TransactionFactory},
@@ -43,11 +44,13 @@ enum Command {
     /// Create an account on the blockchain
     InitMultiToken {},
 
-    RegisterUser {
-    },
+    RegisterUser {},
 
+    MintBarsNft {},
 
-    MintBarsNft {
+    CreateAccount {
+        new_account_address: String,
+        new_auth_key_prefix: String,
     },
 
     /// Transfer a BARS NFT
@@ -79,17 +82,39 @@ async fn main() -> Result<()> {
 
     match args.cmd {
         Command::InitMultiToken { .. } => init_multi_token(&mut account, &client)?,
-        Command::RegisterUser { .. } => {
-            register_user(&mut account, &client)?
-        }
-        Command::MintBarsNft { .. } => {
-            mint_bars_nft(&mut account, &client)?
-        }
+        Command::RegisterUser { .. } => register_user(&mut account, &client)?,
+        Command::MintBarsNft { .. } => mint_bars_nft(&mut account, &client)?,
         Command::TransferBarsNft { .. } => {
             // transfer_bars_nft()
         }
+        Command::CreateAccount {
+            new_account_address,
+            new_auth_key_prefix,
+        } => create_account(&mut account, &client, new_account_address, new_auth_key_prefix)?,
     }
 
+    Ok(())
+}
+
+fn create_account(
+    account: &mut LocalAccount,
+    client: &BlockingClient,
+    new_address: String,
+    new_auth_key_prefix: String,
+) -> Result<()> {
+    let txn =
+        account.sign_with_transaction_builder(TransactionFactory::new(ChainId::test()).payload(
+            stdlib::encode_create_parent_vasp_account_script_function(
+                xus_tag(),
+                0,
+                AccountAddress::from_hex_literal(&new_address).unwrap(),
+                hex::decode(&new_auth_key_prefix).unwrap(),
+                Vec::new(),
+                true,
+            ),
+        ));
+    send(&client, txn)?;
+    println!("Success");
     Ok(())
 }
 
@@ -114,10 +139,12 @@ fn register_user(account: &mut LocalAccount, client: &BlockingClient) -> Result<
 }
 
 fn mint_bars_nft(account: &mut LocalAccount, client: &BlockingClient) -> Result<()> {
-
     let txn = account.sign_with_transaction_builder(
-        TransactionFactory::new(ChainId::test())
-            .payload(stdlib::encode_mint_bars_script_function("Ankush".to_string().as_bytes().to_vec(), "diem.com".to_string().as_bytes().to_vec(), 100)),
+        TransactionFactory::new(ChainId::test()).payload(stdlib::encode_mint_bars_script_function(
+            "Ankush".to_string().as_bytes().to_vec(),
+            "diem.com".to_string().as_bytes().to_vec(),
+            100,
+        )),
     );
     send(&client, txn)?;
     println!("Success");
